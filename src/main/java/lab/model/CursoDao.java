9539001b157f0e7e0fd0ff4aa5850704db7e192b
Walison Moreira <walison.moreira@gmail.com>
2014-06-06 20:47:27 -0300

    Listagem de cursos.

--- src/main/java/lab/model/CursoDao.java
+++ src/main/java/lab/model/CursoDao.java
@@ -1,5 +1,9 @@
 package lab.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,11 +13,46 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 @ApplicationScoped
 public class CursoDao {
+
+	public CursoDao() {
+		try {
+			//Carrega o driver do banco de dados.
+			Class.forName("org.apache.derby.jdbc.ClientDriver");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 	private List<Curso> cursoDataSource = new ArrayList<Curso>();
 	
 	public List<Curso> obterCursos() {
-		//Esse código deve realizar uma pesquisa no banco de dados. 
-		return cursoDataSource;
+		//Cria uma lista de cursos.
+		List<Curso> cursos = new ArrayList<Curso>();
+		try {
+			// URL de conexão JDBC com o banco de dados.
+			String url = "jdbc:derby://localhost/db";
+			// Obtém uma conexão com o banco de dados.
+			Connection conn = DriverManager
+					.getConnection(url, "app", "123");
+			//Obtém uma sentença SQL.
+			Statement stmt = conn.createStatement();
+			//Executa a instrução SQL.
+			ResultSet rs = stmt
+					.executeQuery("select codigo, nome from curso");
+			//Percorre todos os registros.
+			while (rs.next()) {
+				String codigo = rs.getString("codigo");
+				String nome = rs.getString("nome");
+
+				Curso curso = new Curso();
+				curso.setCodigo(codigo);
+				curso.setNome(nome);
+				cursos.add(curso);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return cursos;
 	}
 	
 	public void incluirCurso(Curso curso) {
